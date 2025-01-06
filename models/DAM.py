914c8d91a76231eab1c4db43b4795c7e22b89477
sharjeel1999 <sharjeelmasood10b15@gmail.com>
2025-01-06 18:04:27 +0900

    added DAM code from repo

---
+++ models/DAM.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+
+from DAM_core import HopfieldCore
+
+class Continous_DAM(nn.Module):
+    def __init__(self)
--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from models.classical_hopfield import Classical_HN
 def build_model(args, weights_folder, visual_folder):
     if args.model == 'classical':
         return Classical_HN(args, weights_folder, visual_folder)
+    elif args.model == 'DAM':
+        return 0
